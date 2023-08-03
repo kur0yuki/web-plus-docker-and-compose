f9fb498eca40cb2e7b03acc6c7c0dfe705270ca8
Catherine <cath.osipova@gmail.com>
2023-08-03 19:18:18 +0300

    started docker-compose

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -1,13 +1,16 @@
 import { DataSource } from 'typeorm';
+require('dotenv').config({path: '.env'});
+
+const { DATABASE_URL, DATABASE_PORT, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_SCHEMA } = process.env;
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'pguser',
-  password: 'qwerty',
-  database: 'kupipodariday',
-  schema: 'kupipodariday',
+  host: DATABASE_URL,
+  port: +DATABASE_PORT,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
+  schema: DATABASE_SCHEMA,
   entities: ['src/**/entities/*.entity{.ts,.js}'],
   migrations: ['src/database/migrations/*.ts'],
   synchronize: false,
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,13 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const { port = 3001 } = process.env;
+
   app.use(cookieParser());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   //app.use(helmet);
-  await app.listen(3001);
+  await app.listen(port);
 }
 bootstrap();
